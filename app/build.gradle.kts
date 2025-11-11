3adb9bb33c252e63ff6a5188f2c6bffb0229d607
Davi Freitas <davicfreitas1@gmail.com>
2025-11-11 10:20:47 -0300

    Refactor: Configuração das bibliotecas do maps.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20"
+    id ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -39,7 +40,9 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
+    buildToolsVersion = "36.1.0"
 }
 
 dependencies {
@@ -62,9 +65,17 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-    implementation("androidx.navigation:navigation-compose:2.9.5")
+    implementation("androidx.navigation:navigation-compose:2.9.6")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
+
+    // Google maps
+    implementation("com.google.android.gms:play-services-maps:19.2.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+
+    // Google maps for compose
+    implementation("com.google.maps.android:maps-compose:6.12.1")
+
 
 }
